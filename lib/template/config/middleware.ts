b590cfc9fdac8f5f0861bd7c02f57c12dee528eb
richen <richenlin@gmail.com>
2019-10-21 13:26:03 +0800

    Fixbug

--- lib/template/config/middleware.ts
+++ lib/template/config/middleware.ts
@@ -2,12 +2,12 @@
  * @ author: xxx
  * @ copyright: Copyright (c)
  * @ license: Apache License 2.0
- * @ version: 2019-10-12 14:11:06
+ * @ version: 2019-10-21 10:45:53
  */
 export default {
     list: [], //加载的中间件列表
     config: { //中间件配置
-        // static: {
+        // Static: {
         //     cache: false
         // },
     }
