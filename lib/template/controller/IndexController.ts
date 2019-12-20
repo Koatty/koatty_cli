c85dbdf5b93e7bbe4dfce3be61699e8ccd2dcdd0
richen <richenlin@gmail.com>
2019-12-20 09:18:34 +0800

    Update

--- lib/template/controller/IndexController.ts
+++ lib/template/controller/IndexController.ts
@@ -2,7 +2,7 @@
  * @ author: xxx
  * @ copyright: Copyright (c)
  * @ license: Apache License 2.0
- * @ version: 2019-10-30 15:27:21
+ * @ version: 2019-12-18 09:49:09
  */
 import { Controller, BaseController, GetMaping, PathVariable, RequestBody } from "koatty";
 import { App } from '../App';
@@ -11,6 +11,10 @@ import { App } from '../App';
 export class IndexController extends BaseController {
     app: App;
 
+    /**
+     * Custom constructor
+     *
+     */
     init() {
         //...
     }
