fed877f8abdd60c806c15d27889e003752eb726c
richen <richenlin@gmail.com>
2019-12-31 10:05:28 +0800

    Use class-validator for entity class validation

---
+++ lib/template/dto/NewDTO.ts
@@ -0,0 +1,13 @@
+/**
+ * @ author: richen
+ * @ copyright: Copyright (c) - <richenlin(at)gmail.com>
+ * @ license: MIT
+ * @ version: 2019-12-30 20:45:14
+ */
+import { IsNotEmpty } from "koatty";
+import { App } from '<Path>/App';
+
+export class <DTO> {
+    // @IsNotEmpty()
+    // name: string;
+}
