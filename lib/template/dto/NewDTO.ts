b3ba7c26889595f47ee770652542790fb22290eb
richen <richenlin@gmail.com>
2019-12-31 16:33:33 +0800

    Update

--- lib/template/dto/NewDTO.ts
+++ lib/template/dto/NewDTO.ts
@@ -2,10 +2,9 @@
  * @ author: richen
  * @ copyright: Copyright (c) - <richenlin(at)gmail.com>
  * @ license: MIT
- * @ version: 2019-12-30 20:45:14
+ * @ version: 2019-12-31 11:20:09
  */
 import { IsNotEmpty } from "koatty";
-import { App } from '<Path>/App';
 
 export class <DTO> {
     // @IsNotEmpty()
