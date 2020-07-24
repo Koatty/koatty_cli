1062ac2a037795cd5e567e10d41bff5d4e1e5bb7
linyyyang <linyyyang@tencent.com>
2020-07-24 15:57:49 +0800

    add plugin cmd

---
+++ lib/template/plugin/NewPlugin.ts
@@ -0,0 +1,15 @@
+/**
+ * @ author: xxx
+ * @ copyright: Copyright (c)
+ * @ license: Apache License 2.0
+ * @ version: 2020-05-18 11:36:51
+ */
+import { Plugin, IPlugin, Helper } from "koatty";
+import { App } from '<Path>/App';
+
+@Plugin()
+export class <PLUG> implements IPlugin {
+    run(options: any, app: App) {
+        // todo...
+    }
+}
