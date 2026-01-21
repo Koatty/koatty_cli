38fdc8de6461b90b671750975ec8c522f785cd46
linyyyang <linyyyang@tencent.com>
2026-01-21 17:13:29 +0800

    Task 9 完成: 统一错误提示与帮助 (interactive.js)

--- src/utils/interactive.js
+++ src/utils/interactive.js
@@ -1,28 +1,44 @@
 const inquirer = require("inquirer");
 
 async function promptInput(message, defaultVal) {
-  const { answer } = await inquirer.prompt([
-    {
-      type: "input",
-      name: "answer",
-      message,
-      default: defaultVal || "",
-    },
-  ]);
-  return answer;
+  try {
+    const { answer } = await inquirer.prompt([
+      {
+        type: "input",
+        name: "answer",
+        message,
+        default: defaultVal || "",
+      },
+    ]);
+    return answer;
+  } catch (error) {
+    if (error.isTtyError) {
+      console.log("Prompt couldn't be rendered in the current environment");
+    } else {
+      process.exit(0);
+    }
+  }
 }
 
 async function promptSelect(message, choices, defaultVal) {
-  const { answer } = await inquirer.prompt([
-    {
-      type: "list",
-      name: "answer",
-      message,
-      choices,
-      default: defaultVal || choices[0],
-    },
-  ]);
-  return answer;
+  try {
+    const { answer } = await inquirer.prompt([
+      {
+        type: "list",
+        name: "answer",
+        message,
+        choices,
+        default: defaultVal || choices[0],
+      },
+    ]);
+    return answer;
+  } catch (error) {
+    if (error.isTtyError) {
+      console.log("Prompt couldn't be rendered in the current environment");
+    } else {
+      process.exit(0);
+    }
+  }
 }
 
 module.exports = {
